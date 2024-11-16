af8cb48341332f6ab1cf88ccc09d159476ba5be1
rohan-depoter <rohan.bhatia@depoter.com>
2024-11-16 16:02:09 +0530

    added params feature

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,49 +11,55 @@ export type MiddlewareFunction = (
 ) => (req: Request, res: Response, next: NextFunction) => void;
 
 type ValidationSchema = {
-  [key: string]: { [field: string]: Validator | Validator[] };
+  [routePattern: string]: { [field: string]: Validator | Validator[] };
 };
 
-// This will be used as the validator middleware for global use
 export const validateRequest = (options: ValidationSchema) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    const route = req.originalUrl; // Get the current route
+    // Get the current route path
+    const route = req.route.path; // This gives you the path from the express router
 
-    // Check if validation rules exist for the current route
-    if (options[route]) {
-      const targetData = req.body; // Default target is `body`
-      const errors: { [key: string]: string } = {};
+    // Find a matching schema for the route
+    for (const routePattern in options) {
+      // Match dynamic parameters with regex
+      const regex = new RegExp(`^${routePattern.replace(/:\w+/g, "\\w+")}$`);
+      if (regex.test(route)) {
+        const schema = options[routePattern];
+        const targetData = req.body; // Default target is `body`
+        const errors: { [key: string]: string } = {};
 
-      // Get the schema for the current route
-      const schema = options[route];
+        // If route has params, check req.params for validation
+        const target = req.params || targetData;
 
-      // Iterate over each field in the schema and validate it
-      for (const field in schema) {
-        if (Object.prototype.hasOwnProperty.call(schema, field)) {
-          let validators: Validator[] = [];
+        // Iterate over each field in the schema and validate it
+        for (const field in schema) {
+          if (Object.prototype.hasOwnProperty.call(schema, field)) {
+            let validators: Validator[] = [];
 
-          // If multiple validators are defined for a field, handle them
-          if (Array.isArray(schema[field])) {
-            validators = schema[field] as Validator[];
-          } else {
-            validators = [schema[field] as Validator];
-          }
+            if (Array.isArray(schema[field])) {
+              validators = schema[field] as Validator[];
+            } else {
+              validators = [schema[field] as Validator];
+            }
 
-          // Iterate through each validator
-          for (const validator of validators) {
-            const error = validator(targetData[field], field); // Validate field
+            // Iterate through each validator
+            for (const validator of validators) {
+              const error = validator(target[field], field);
 
-            if (error) {
-              errors[field] = error; // Collect errors
-              break; // Stop further checks if error found for this field
+              if (error) {
+                errors[field] = error;
+                break; // Stop further checks if error found for this field
+              }
             }
           }
         }
-      }
 
-      // If there are any validation errors, return a 400 response with the errors
-      if (Object.keys(errors).length > 0) {
-        return res.status(400).json({ errors });
+        // If there are any validation errors, return a 400 response with the errors
+        if (Object.keys(errors).length > 0) {
+          return res.status(400).json({ errors });
+        }
+
+        break; // Exit loop if a matching route is found
       }
     }
 
